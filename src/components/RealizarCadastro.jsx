e0076f68d58d49ab50020a00df51887102cbd917
giovannaarrais <giovannaarrais13@gmail.com>
2025-08-03 20:57:49 -0300

    :lipstick: style: ajuste de responsividade em tela de login no mobile

--- src/components/RealizarCadastro.jsx
+++ src/components/RealizarCadastro.jsx
@@ -22,7 +22,7 @@ function RealizarCadastro(  ){
     }
 
     return (
-        <div className="flex md:flex-row flex-col-reverse h-screen">
+        <div className="flex md:flex-row flex-col-reverse ">
             <div className="flex-1 bg-linear-to-b from-sky-500 to-sky-900 py-8 flex items-center justify-center flex-col h-screen">
                 <h1 className="text-2xl text-center font-semibold text-white">
                     Realize seu Cadastro
